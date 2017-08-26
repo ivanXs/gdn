3223d9113a2208273cc44c270382b748a0dcfb56
IvanXs <ivan.siprak@gmail.com>
2017-08-26 13:07:33 +0200

    Init cleaning

--- relay_sw.py
+++ relay_sw.py
@@ -1,16 +1,16 @@
-import RPi.GPIO as GPIO  #Use for Raspberry Pi GPIO
-#import testRPiGPIO as GPIO  # Use for Debugging GPIO
+import RPi.GPIO as GPIO  # Use for Raspberry Pi GPIO
+# import testRPiGPIO as GPIO  # Use for Debugging GPIO
 import time
 import Adafruit_CharLCD as LCD
 from datetime import datetime
 
+# Raspberry Pi BMC pinout configuration:
 
-LedPin = 17    # pin11 --- Control Led Indicator
-RelayPin = 18  # pin12 --- Relay module x1
-BtnPin = 27    # pin13 --- float Sensor Switch
-
-# Raspberry Pi pin configuration:
-lcd_rs = 25  # Note this might need to be changed to 21 for older revision Pi's.
+LedPin = 17  # pin11 --- Control Led Indicator
+RelayPumpPin = 18  # pin12 --- Relay module x1
+BtnPin = 27  # pin13 --- Float Sensor Switch
+# LCD pinout
+lcd_rs = 25
 lcd_en = 24
 lcd_d4 = 23
 lcd_d5 = 5
@@ -25,11 +25,11 @@ lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
 
 
 def setup():
-    GPIO.setmode(GPIO.BCM)          # Numbers pins by physical location
-    GPIO.setup(RelayPin, GPIO.OUT)    # Set pin mode as output
-    GPIO.output(RelayPin, GPIO.HIGH)   # relay off
-    GPIO.setup(LedPin, GPIO.OUT)      # Set pin mode as output
-    GPIO.output(LedPin, GPIO.LOW)    # Set led off
+    GPIO.setmode(GPIO.BCM)  # Numbers pins by BMC
+    GPIO.setup(RelayPumpPin, GPIO.OUT)  # Set pump relay pin mode as output
+    GPIO.output(RelayPumpPin, GPIO.HIGH)  # Set pump relay OFF
+    GPIO.setup(LedPin, GPIO.OUT)  # Set control LED pin mode as output
+    GPIO.output(LedPin, GPIO.LOW)  # Set control LED OFF
     GPIO.setup(BtnPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Set BtnPin's mode is input, and pull up to high level(3.3V)
     message = 'Demo x01'
     lcd.message(message)
@@ -39,7 +39,6 @@ def setup():
     for i in range(lcd_columns - len(message)):
         time.sleep(0.5)
         lcd.move_left()
-
 
 
 def loop():
@@ -65,7 +64,7 @@ def pokreni_punjenje():
     """
     print('Pokrecem punjenje...')
     GPIO.output(LedPin, GPIO.HIGH)  # led on
-    GPIO.output(RelayPin, GPIO.LOW)  # rel on
+    GPIO.output(RelayPumpPin, GPIO.LOW)  # rel on
     if status_punjenja():
         print('...Signal punjenja prisutan')
         try:
@@ -73,7 +72,7 @@ def pokreni_punjenje():
             lcd.clear()
             while GPIO.input(BtnPin) == GPIO.LOW:
                 print('Punim...')
-                lcd.message(scroll_msg+'\n')
+                lcd.message(scroll_msg + '\n')
                 lcd.message(datetime.now().strftime('%b %d  %H:%M:%S'))
                 time.sleep(2)
             if zavrsi_punjenje():
@@ -94,7 +93,7 @@ def pokreni_punjenje():
 def zavrsi_punjenje():
     print('Punjenje zavrseno, zatvaram ventil...')
     GPIO.output(LedPin, GPIO.LOW)  # led off
-    GPIO.output(RelayPin, GPIO.HIGH)  # rel off
+    GPIO.output(RelayPumpPin, GPIO.HIGH)  # rel off
     if not (status_punjenja()):
         print('...Signal zatvaranja predan')
         return True
@@ -119,7 +118,7 @@ def display():
 
 
 def destroy():
-    GPIO.output(RelayPin, GPIO.HIGH)
+    GPIO.output(RelayPumpPin, GPIO.HIGH)
     lcd.clear()
     GPIO.cleanup()  # Release resource
 
